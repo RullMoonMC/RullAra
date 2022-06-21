caee87539afcbffe3ad15af217448b0705099411
RullMoonMC <107551141+RullMoonMC@users.noreply.github.com>
2022-06-21 08:31:46 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -10,9 +10,9 @@ let text = `
 ❏────
 `
 const templateButtons = [
-    {index: 1, urlButton: {displayText: '✨ Saweria', url: psaweria}},
-    {index: 2, urlButton: {displayText: '📷 Instagram', url: sig}},
-    {index: 3, urlButton: {displayText: '🌎 Official Group', url: sgc}},
+    {index: 1, urlButton: {displayText: '🦋 Saweria', url: psaweria}},
+    {index: 2, urlButton: {displayText: '🔮 Instagram', url: sig}},
+    {index: 3, urlButton: {displayText: '⛰️ Official Group', url: sgc}},
     {index: 4, quickReplyButton: {displayText: 'Menu', id: '.menu'}},
     {index: 5, quickReplyButton: {displayText: 'Owner', id: '.owner'}},
 ]
