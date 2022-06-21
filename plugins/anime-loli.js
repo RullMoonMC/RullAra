5937eb58509196b1d8c4877632209513167edbf8
RullMoonMC <107551141+RullMoonMC@users.noreply.github.com>
2022-06-21 12:37:06 +0700

    Update anime-loli.js

--- plugins/anime-loli.js
+++ plugins/anime-loli.js
@@ -2,7 +2,7 @@ import fetch from 'node-fetch'
 
 let handler = async (m, { conn }) => {
 	let url = loli[Math.floor(Math.random() * loli.length)]
-	conn.sendButton(m.chat, 'Tch, dasar pedofil', wm, url, [['Next','.loli']],m)
+	conn.sendButton(m.chat, '💧﹒∯﹒Loli✨﹐⁂', wm, url, [['Next','.loli']],m)
 }
 handler.command = /^(loli)$/i
 handler.tags = ['anime']
