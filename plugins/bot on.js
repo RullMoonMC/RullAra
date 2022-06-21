72d2161aacc2f228fa3ec976c7cad9ae652cfd87
RullMoonMC <107551141+RullMoonMC@users.noreply.github.com>
2022-06-21 08:08:21 +0700

    Update bot on.js

--- plugins/bot on.js
+++ plugins/bot on.js
@@ -1,9 +1,10 @@
 let handler = async (m) => {
     global.db.data.chats[m.chat].isBanned = false
-    m.reply(' *Uaaaaawh🥱, Maap kak Kanna tadi ketiduran😊* ')
-}
+    m.reply(' *Uaaaaawh🥱, Maap kak Moon tadi ketiduran😴* ')
+}
+
 handler.tags = ['main']
-handler.command = /^(kannaon)$/i
+handler.command = /^(moonon)$/i
 
 handler.admin = true
 
